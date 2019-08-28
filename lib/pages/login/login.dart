9ca06a58502a1484eaf536226380e4d5dec24715
gaoshanshan <gaoshanshan@gaoshanshandeMacBook-Pro.local>
2019-08-28 10:32:35 +0800

    登录框设置InkWell

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
+import 'regist.dart';
 
 class LoginWidget extends StatefulWidget {
   LoginWidget({Key key}) : super(key: key);
@@ -9,6 +10,17 @@ class LoginWidget extends StatefulWidget {
 
 class _LoginWidgetState extends State<LoginWidget> {
   TextStyle defaultStyle = TextStyle(color: Colors.white);
+  String username = '';
+  String password = '';
+
+  registUser() {
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) {
+        return RegistWidget();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,98 +28,101 @@ class _LoginWidgetState extends State<LoginWidget> {
             width: double.infinity,
             height: double.infinity,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/images/loginbackground.png'))),
+                image: DecorationImage(fit: BoxFit.cover, image: AssetImage('assets/images/loginbackground.png'))),
             child: Stack(children: <Widget>[
               Positioned(
                   width: MediaQuery.of(context).size.width,
-                  bottom: 20,
-                  child: Align(
-                      child: Text('生活不止眼前的...还有远方',
-                          style: TextStyle(color: Colors.grey, fontSize: 10)))),
+                  bottom: 10,
+                  child: Align(child: Text('生活不止眼前的...还有远方', style: TextStyle(color: Colors.grey, fontSize: 10)))),
               Positioned(
                   width: MediaQuery.of(context).size.width,
-                  bottom: 60,
+                  bottom: 30,
                   child: Align(
-                      child: GestureDetector(
-                    child: Icon(FontAwesomeIcons.weixin, color: Colors.white),
-                    onTap: () {
-                      print('hello');
-                    },
-                  ))),
+                    child: IconButton(
+                      icon: Icon(FontAwesomeIcons.weixin, color: Colors.white),
+                      onPressed: () {
+                        print('login with weixin');
+                      },
+                    ),
+                  )),
               Positioned(
                   width: MediaQuery.of(context).size.width,
-                  bottom: 150,
+                  bottom: 120,
                   child: Align(
                       child: Column(
                     children: <Widget>[
                       Container(
                         width: MediaQuery.of(context).size.width - 50,
-                        height: 70,
+                        height: 60,
                         child: TextField(
                           style: defaultStyle,
                           decoration: InputDecoration(
-                              enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.white, width: 2)),
-                              hintText: '用户名或手机号',
+                              enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 2)),
+                              hintText: '手机号',
                               hintStyle: defaultStyle,
                               focusColor: Colors.white,
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.white, width: 2))),
+                              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 2))),
+                          onChanged: (String value) {
+                            setState(() {
+                              username = value;
+                            });
+                          },
                         ),
                       ),
                       Container(
                         width: MediaQuery.of(context).size.width - 50,
-                        height: 70,
+                        height: 60,
                         child: TextField(
                           style: defaultStyle,
                           //校验密码
                           decoration: InputDecoration(
-                              enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.white, width: 2)),
+                              enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 2)),
                               hintText: '密码',
                               hintStyle: defaultStyle,
                               focusColor: Colors.white,
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.white, width: 2))),
+                              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 2))),
                           obscureText: true,
+                          onChanged: (String value) {
+                            setState(() {
+                              password = value;
+                            });
+                          },
                         ),
                       ),
                       SizedBox(height: 20),
-                      InkWell(
-                        child: Container(
-                            width: MediaQuery.of(context).size.width - 50,
-                            height: 40,
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Color.fromRGBO(121, 121, 121, 1)),
-                                color: Color.fromRGBO(242, 242, 242, 0.09),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(5))),
-                            child:
-                                Align(child: Text('登录', style: defaultStyle))),
-                      ),
+                      Material(
+                          color: Color.fromRGBO(0, 0, 0, 0),
+                          child: InkWell(
+                              splashColor: Color.fromRGBO(242, 242, 242, 0.5),
+                              highlightColor: Color.fromRGBO(0, 0, 0, 0),
+                              child: Container(
+                                  width: MediaQuery.of(context).size.width - 50,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Color.fromRGBO(121, 121, 121, 1)),
+                                      color: Color.fromRGBO(242, 242, 242, 0.09),
+                                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                                  child: Align(child: Text('登录', style: defaultStyle))),
+                              onTap: () {
+                                print('$username:$password');
+                              })),
                       SizedBox(height: 10),
                       Container(
                           width: MediaQuery.of(context).size.width - 50,
                           height: 40,
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text('没有用户名?', style: defaultStyle),
-                                Text(' ', style: defaultStyle),
-                                InkWell(
-                                    child: Text('注册',
-                                        style: TextStyle(
-                                          color: Color.fromRGBO(2, 167, 240, 1),
-                                          fontWeight: FontWeight.bold,
-                                        )))
-                              ])),
+                          child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                            Text('没有账号?', style: defaultStyle),
+                            Text(' ', style: defaultStyle),
+                            InkWell(
+                                child: Text('注册',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(2, 167, 240, 1),
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                onTap: () {
+                                  registUser();
+                                })
+                          ])),
                     ],
                   )))
             ])));
